4a324561463ac4b3f28340e5730cd6e599c12374
santiago reverter <sreverter@outlook.com>
2022-09-04 20:07:07 -0300

    correccion de boris aplicada habiendo dejado bien ubicado el map y no hardcodeado el item

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,10 +1,29 @@
 import CardItem from "../CardItem";
+import { useEffect, useState } from "react";
 
 const CardList = () =>{
+
+    const [productos, setProductos] = useState([]);
+
+    const buscarProductos = async () => {
+        try{
+            const response = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=relojes');
+            const data = await response.json();
+            setProductos(data.results);
+        } catch(err){
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        buscarProductos();
+    },[])
+
     return(
         <div style={{display: 'flex', flexDirection: 'row', wrap: 'nowrap'}}>
             <ul style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
-                <CardItem />
+                {productos.map((producto, index) => {
+                    return <CardItem producto={producto} />
+                })}
             </ul>
         </div>
     );
